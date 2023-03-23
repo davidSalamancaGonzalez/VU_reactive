a4f3025d2f1b04dedb788ff83960e66074f8b920
davsalgonz <david.slmnc@gmail.com>
2023-03-23 18:30:49 +0100

    reactive andorra and spain

--- src/app/view/form/form.component.ts
+++ src/app/view/form/form.component.ts
@@ -5,39 +5,35 @@ import { ResultService } from '../../service/result.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
-  result: string = ""
+  result: string = '';
   amountInvested: number = 0;
 
-  constructor(private fb: FormBuilder,
-              private resultCalc : ResultService) { }
+  constructor(private fb: FormBuilder, private resultCalc: ResultService) {}
 
- myForm : FormGroup = this.fb.group({
-    taxBase: [ , [Validators.required]],
-    pensionPlan: [ , [Validators.required, Validators.max(1500)]],
-    pensionPlanComp: [ , [Validators.required , Validators.max(8500)]],
-  })
+  myForm: FormGroup = this.fb.group({
+    taxBase: [, [Validators.required]],
+    pensionPlan: [, [Validators.required, Validators.max(1500)]],
+    pensionPlanComp: [, [Validators.required, Validators.max(8500)]],
+    inlineRadioOptions: [, [Validators.required]],
+  });
 
-calculate(){
-
-  if(this.myForm.valid){
-    this.resultCalc.calculate(this.myForm.value)
-    this.result = this.resultCalc.totalDiscount.toFixed(2)
-    this.myForm.reset()
-  } else {
-    this.myForm.markAllAsTouched()
+  calculate() {
+    if (this.myForm.valid) {
+      this.resultCalc.calculate(this.myForm.value);
+      this.result = this.resultCalc.totalDiscount.toFixed(2);
+      this.myForm.reset();
+    } else {
+      this.myForm.markAllAsTouched();
+    }
   }
-}
 
-validField(field: string){
-  return this.myForm.controls[field].errors && this.myForm.controls[field].touched
-}
-
-test(){
-
-  console.log(this.myForm)
-}
+  validField(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
 
 }
