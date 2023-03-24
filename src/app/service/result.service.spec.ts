cc634621fccb4b63d852ef3f9aa662f19e5022bc
davsalgonz <david.slmnc@gmail.com>
2023-03-24 10:03:56 +0100

    test done

--- src/app/service/result.service.spec.ts
+++ src/app/service/result.service.spec.ts
@@ -14,4 +14,30 @@ describe('ResultService', () => {
     expect(service).toBeTruthy();
   });
 
+  describe('calculate', () => {
+    it('should calculate the totalDiscount for Spain correctly', () => {
+      const formValues = {
+        taxBase: 30000,
+        pensionPlan: 1500,
+        pensionPlanComp: 8500,
+        inlineRadioOptions: 'ESP'
+      };
+      service.calculate(formValues);
+      expect(service.totalDiscount).toBe(3000);
+    });
+
+    it('should calculate the totalDiscount for Andorra correctly', () => {
+      const formValues = {
+        taxBase: 30000,
+        pensionPlan: 1500,
+        pensionPlanComp: 8500,
+        inlineRadioOptions: 'AND'
+      };
+      service.calculate(formValues);
+      expect(service.totalDiscount).toBe(500);
+    });
+
+
+  });
+
 });
--- src/app/view/form/form.component.spec.ts
+++ src/app/view/form/form.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from './form.component';
+import { ResultService } from '../../service/result.service';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let resultService: ResultService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ FormComponent ],
+      imports: [ ReactiveFormsModule ],
+      providers: [ ResultService ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    resultService = TestBed.inject(ResultService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should calculate the result', () => {
+    const formValues = {
+      taxBase: 30000,
+      pensionPlan: 1500,
+      pensionPlanComp: 8500,
+      inlineRadioOptions: 'ESP'
+    };
+    spyOn(resultService, 'calculate').and.callThrough();
+    spyOn(component.myForm, 'reset');
+    component.myForm.setValue(formValues);
+    component.calculate();
+    expect(resultService.calculate).toHaveBeenCalledWith(formValues);
+    expect(component.result).toBe('3000.00');
+    expect(component.myForm.reset).toHaveBeenCalled();
+  });
+
+  it('should not calculate the result if the form is invalid', () => {
+    const formValues = {
+      taxBase: 30000,
+      pensionPlan: 3000, // exceeds max limit
+      pensionPlanComp: 5000,
+      inlineRadioOptions: 'ESP'
+    };
+    spyOn(resultService, 'calculate');
+    component.myForm.setValue(formValues);
+    component.calculate();
+    expect(resultService.calculate).not.toHaveBeenCalled();
+    expect(component.result).toBe('');
+    expect(component.myForm.controls['pensionPlan'].valid).toBe(false);
+  });
+
+
 });
