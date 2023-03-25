e891fe35fe1dafc24768aa67f0dc35d1e08e0b2a
davsalgonz <david.slmnc@gmail.com>
2023-03-25 10:48:42 +0100

    add home & router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './view/form/form.component';
+import { HomeComponent } from './view/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'reactive-forms', component: FormComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
